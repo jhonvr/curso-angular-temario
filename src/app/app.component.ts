dae5c306a8c08f11498c98afc5fb9ebc1a39a053
deividnew94 <deividnew94@gmail.com>
2018-02-26 21:28:44 -0500

    tipos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title: string;
+  isHappy: boolean = false;
+  numberOne: number = 1;
+  numberTwo:number = 2;
+  name : string ='Jhonatan';
+  logo: string='assets/images/logo.svg'
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
+
+
+  constructor(){
+    this.title = 'Curso Angular';
+  }
+
+  handleClick(){
+    this.name="Amadeo";
+  }
+
+  handleInput(event:any){
+    this.name = event.target.value;
+  }
+
+  handleBlur(event:any){
+    this.name = event.target.value;
+  }
+
+  handleChange(value:string){
+    this.name=value;
+  }
+
+  handleClickRefs(value:string){
+    console.log(value);
+  }
+
+
 }
