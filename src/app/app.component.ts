91c85e5bfbdde40e0fc84d0f64bdd19a481a8877
deividnew94 <deividnew94@gmail.com>
2018-02-26 21:33:03 -0500

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ interface Passenger {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
